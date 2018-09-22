54b413346c0dfc92c7f0e73d2d93fc62d1dcf446
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2018-09-22 15:35:25 -0600

    Commit inicial

---
+++ topicos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(topicos.Startup))]
+namespace topicos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
